80a39a414010bfdaa9565cd54d316dec8f7377b0
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-17 04:51:05 +0000

    [Add]顧客、管理者のcontolle,view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :customers,skip: [:passwords],controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  
+  devise_for :admin,skip: [:registrations, :passwords],controllers: {
+  sessions: "admin/sessions"
+}
+
   namespace :public do
     get 'items/index'
     get 'items/show'
