887ff04338f23821eb291ae544bd2a166c1ade7a
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-17 04:30:34 +0000

    [Add]devise Customer,devise Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :customers
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
