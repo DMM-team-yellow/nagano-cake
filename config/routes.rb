c749b9ffe6c8aff8779cb99f06d8e5d6d22c0567
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-17 07:32:52 +0000

    [Add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,30 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_items/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/crate'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
   devise_for :customers,skip: [:passwords],controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  
+
   devise_for :admin,skip: [:registrations, :passwords],controllers: {
   sessions: "admin/sessions"
 }
 
   namespace :public do
-    get 'items/index'
-    get 'items/show'
+    resources :homes, only[:top,:about]
+    resources :items, only[:index,:show]
+    resources :customers, only[:show,:edit,:update]
+    resources :cart_items, only[:index,:update,:destroy,:destroy_all,:create]
+    resources :orders, only[:new,:index,:show,:create]
+    resources :addresses, only[:index,:edit,:create,:update,:destroy]
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+  
+  namespace :admin do
+    resources :homes, only[:top]
+    resources :items, only[:new,:index,:show,:create,:edit,:update]
+    resources :genres, only[:index,:create,:edit,:update]
+    resources :customers, only[:index,:show,:edit,:update]
+    resources :orders, only[:show,:update]
+    resources :order_items, only[:update]
   end
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
