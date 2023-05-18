41e13542295680c20e1de838521674fefb2a7dd6
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-18 01:43:50 +0000

    [Fix]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,25 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+
+  root to: 'public/homes#top'
   namespace :public do
-    resources :homes, only[:top,:about]
-    resources :items, only[:index,:show]
-    resources :customers, only[:show,:edit,:update]
-    resources :cart_items, only[:index,:update,:destroy,:destroy_all,:create]
-    resources :orders, only[:new,:index,:show,:create]
-    resources :addresses, only[:index,:edit,:create,:update,:destroy]
+    get '/about' => 'public/homes#about'
+    resources :items, only:[:index,:show]
+    resources :customers, only:[:show,:edit,:update]
+    resources :cart_items, only:[:index,:update,:destroy,:destroy_all,:create]
+    resources :orders, only:[:new,:index,:show,:create]
+    resources :addresses, only:[:index,:edit,:create,:update,:destroy]
   end
-  
+
   namespace :admin do
-    resources :homes, only[:top]
-    resources :items, only[:new,:index,:show,:create,:edit,:update]
-    resources :genres, only[:index,:create,:edit,:update]
-    resources :customers, only[:index,:show,:edit,:update]
-    resources :orders, only[:show,:update]
-    resources :order_items, only[:update]
+    get '/admin' => 'admin/homes#top'
+    resources :items, only:[:new,:index,:show,:create,:edit,:update]
+    resources :genres, only:[:index,:create,:edit,:update]
+    resources :customers, only:[:index,:show,:edit,:update]
+    resources :orders, only:[:show,:update]
+    resources :order_items, only:[:update]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
