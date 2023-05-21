91a815f3c593479eebb75cdc05c01744c6afc366
EC2 Default User <ec2-user@ip-172-31-4-80.ap-northeast-1.compute.internal>
2023-05-21 07:43:20 +0000

    [add]顧客商品一覧、商品詳細画面”

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,7 +11,7 @@ class Public::CartItemsController < ApplicationController
     if @cart_item
       @cart_item.quantity += CartItem.new(cart_item_params).quantity
     else
-      @cart_item = CartItem(cart_item_params)
+      @cart_item = CartItem.new(cart_item_params)
     end
     @cart_item.customer_id = current_customer.id
     @cart_item.save
@@ -39,7 +39,7 @@ class Public::CartItemsController < ApplicationController
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :quantity)
+    params.require(:cart_item).permit(:item_id, :quantity, :price)
   end
 
 end
