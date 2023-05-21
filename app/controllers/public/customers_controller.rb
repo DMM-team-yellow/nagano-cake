3c7bff658f2fd122b4dcb808fc05c5d8d1c3c958
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-05-21 08:48:37 +0000

    [Add]退会処理用のルート

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,12 +10,20 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to customer_path(customer.id)
   end
 
   def unsubscribe
+    @customer = Customer.find(params[:id])
   end
 
-  def withdrawl
+  def withdrawal
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
 
