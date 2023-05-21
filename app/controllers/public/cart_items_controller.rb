117fd849afae8c8f0e9e673e199951b901119879
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-21 05:03:36 +0000

    [Fix]ルーティング

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,11 @@
 class Public::CartItemsController < ApplicationController
-  
+
   def index
     @cart_items = CartItem.all
     @items = Item.all
     @cart_item = @items.all
   end
-  
+
   def create
     @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
     if @cart_item
@@ -17,29 +17,29 @@ class Public::CartItemsController < ApplicationController
     @cart_item.save
     redirect_to cart_items_path
   end
-  
+
   def update
     @cart_item = current_customer
     @item = Item.all
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to cart_item_path
   end
-  
+
   def destroy_all
     CartItem.destroy_all
     current_customer.cart_item.destroy_all
     redirect_to cart_item_path
   end
-  
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :quantity)
   end
-  
+
 end
