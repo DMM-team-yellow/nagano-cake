44969abac4e110e48f4d4a5ce68bb24e903da278
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-22 01:04:27 +0000

    [Fix]カート一覧画面

--- db/migrate/20230517062804_create_cart_items.rb
+++ db/migrate/20230517062804_create_cart_items.rb
@@ -2,9 +2,9 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
 
-      t.integer :quantity, null: false, default: 1
-      t.references :customer, null: false, foreign_key: true
-      t.references :item, null: false, foreign_key: true
+      t.integer :quantity, null: false #個数
+      t.references :customer, null: false, foreign_key: true #顧客ID
+      t.references :item, null: false, foreign_key: true #商品ID
 
       t.timestamps
     end
