2cda345b9b897347133864571c2fdf923ed3d79a
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-05-23 02:43:26 +0000

    [Add]admin顧客編集が画面作成途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,13 +4,25 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-   @customer = Customer.find(params[:id])
+    @customer = Customer.where(id: params[:id])
   end
 
   def edit
+   @customers = Customer.where(id: params[:id])
+   @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    #redirect_to customer_path(customer.id)
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :telephone_number, :email, :is_deleted)
+  end
+
 
 end
