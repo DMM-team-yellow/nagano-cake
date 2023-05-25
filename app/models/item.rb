50853bba52a028fb39c7a22dbf20f078a3e39cfe
EC2 Default User <ec2-user@ip-172-31-4-80.ap-northeast-1.compute.internal>
2023-05-25 06:02:02 +0000

    [add]ジャンル検索機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,10 @@ class Item < ApplicationRecord
   
   has_many :order_items
   has_many :orders, through: :order_items
- 
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
 end
