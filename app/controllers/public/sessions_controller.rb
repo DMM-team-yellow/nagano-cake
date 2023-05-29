29bd9b4e4433b49696be52d895d29630459df5e8
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-05-29 08:37:10 +0000

    [Add]退会済ユーザー処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -36,11 +36,13 @@ class Public::SessionsController < Devise::SessionsController
    def customer_state
      @customer = Customer.find_by(email: params[:customer][:email])
      return if !@customer
-     if @customer.valid_password?(params[:customer][:password])
+     if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+       flash[:notice] = "退会済みです。再度ご登録のうえご利用ください。"
+       redirect_to new_customer_registration_path
+     else
+       after_sign_in_path_for(resource)
      end
    end
-
-
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
